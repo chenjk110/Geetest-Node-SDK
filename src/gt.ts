03596ceb260e54bc70619f4ec6d0381ad9f9406c
TanKingKhun <chenjk110@qq.com>
2021-08-28 19:20:31 +0800

    <feat> Geetest class basic structure.

---
+++ src/gt.ts
@@ -0,0 +1,19 @@
+import { GeetestConfig } from './gt-config'
+
+export class Geetest {
+
+  config = new GeetestConfig
+  
+  constructor() {
+
+  }
+
+  async register() {
+
+  }
+  
+  async validate() {
+
+  }
+
+}
